62a19366d0f177c18d7d0e29941bd34efe0856d0
Mr-developer345 <halirajput16@gmail.com>
2025-02-23 00:18:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("hello World")
+
+
+var age = "i am 15 years old";
+
+console.log(age)
+
+alert(" i am 15 years old")
+
+var track = "you have visited this site 14 times";
+
+console.log(track)
+
+alert("you have visited this site 14 times")
+
+var birthYear = 1990;
+console.log(birthYear)
+
+var visitor = "hamadali";
+console.log(visitor)
+
+var product = "mobile";
+console.log(product)
+
+var quantity = "5";
+console.log(quantity)
